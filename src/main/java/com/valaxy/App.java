1278129d346ea75515e946612875dee78c92cdb7
Shankar <arsr319@gmail.com>
2020-09-30 08:52:01 +0530

    update app.java file

--- src/main/java/com/valaxy/App.java
+++ src/main/java/com/valaxy/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to DevOps Traning!" );
+        System.out.println( "Welcome to git training!" );
     }
 }
