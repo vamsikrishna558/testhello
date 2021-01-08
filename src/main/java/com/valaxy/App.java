5b55fe80b77e17dd13df38edca6c4f520468f99c
vamsikrishna558 <76549862+vamsikrishna558@users.noreply.github.com>
2021-01-08 13:25:55 +0530

    Updated

--- src/main/java/com/valaxy/App.java
+++ src/main/java/com/valaxy/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to git training!" );
+        System.out.println( "Welcome to Devops training!" );
     }
 }
